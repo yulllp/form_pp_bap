c18ddb7c8424f97fd70e9f4fa29086ba88bf62a2
yulllllll <ceoofyulll@gmail.com>
2024-09-26 15:23:44 +0700

    ongoing table

--- app/Http/Controllers/OngoingController.php
+++ app/Http/Controllers/OngoingController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PermintaanPembelian;
 use Illuminate\Http\Request;
 
 class OngoingController extends Controller
 {
     public function index() {
-        return view('ongoing', ['title' => 'On Going']);
+        $data = PermintaanPembelian::with('pt_tujuan')->whereNot('status', 'acc2')->get();
+        return view('ongoing', ['title' => 'On Going', 'datas' => $data]);
     }
 }
--- resources/views/ongoing.blade.php
+++ resources/views/ongoing.blade.php
@@ -7,75 +7,43 @@
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" class="px-6 py-3">
-              Product name
+              No
             </th>
             <th scope="col" class="px-6 py-3">
-              Color
+              No PPI
             </th>
             <th scope="col" class="px-6 py-3">
-              Category
+              Pembelian untuk PT
             </th>
             <th scope="col" class="px-6 py-3">
-              Price
+              Alasan permintaan
             </th>
             <th scope="col" class="px-6 py-3">
-              <span class="sr-only">Edit</span>
+              Status
             </th>
           </tr>
         </thead>
+        @foreach ($datas as $data)
         <tbody>
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Apple MacBook Pro 17"
+            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              {{ $loop->index + 1 }}
+            </td>
+            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              {{ $data->nomor }}
             </th>
-            <td class="px-6 py-4">
-              Silver
+            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              {{ $data->pt_tujuan->name }}
+            </td>
+            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              {{ $data->alasan }}
             </td>
             <td class="px-6 py-4">
-              Laptop
-            </td>
-            <td class="px-6 py-4">
-              $2999
-            </td>
-            <td class="px-6 py-4 text-right">
-              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-            </td>
-          </tr>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Microsoft Surface Pro
-            </th>
-            <td class="px-6 py-4">
-              White
-            </td>
-            <td class="px-6 py-4">
-              Laptop PC
-            </td>
-            <td class="px-6 py-4">
-              $1999
-            </td>
-            <td class="px-6 py-4 text-right">
-              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-            </td>
-          </tr>
-          <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Magic Mouse 2
-            </th>
-            <td class="px-6 py-4">
-              Black
-            </td>
-            <td class="px-6 py-4">
-              Accessories
-            </td>
-            <td class="px-6 py-4">
-              $99
-            </td>
-            <td class="px-6 py-4 text-right">
-              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Check Status</a>
             </td>
           </tr>
         </tbody>
+        @endforeach
       </table>
     </div>
   </section>
